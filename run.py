1e17c7bcfaf59241a917f9b5a81745815ff0b0c0
RJ Murphy <rj.murphy@libertymutual.com>
2023-08-28 15:59:16 -0400

    initial commit of script

---
+++ run.py
@@ -0,0 +1,33 @@
+import csv
+import json
+import subprocess
+
+def post_data_to_api(url, payload):
+    curl_command = ['curl', '-X', 'POST', '-H', 'Content-Type: 
+application/json', '-d', payload, url]
+    subprocess.run(curl_command)
+
+def csv_to_json(csv_file):
+    with open(csv_file, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            # Set variables from CSV data
+            variable1 = row['Column1']
+            variable2 = row['Column2']
+            variable3 = row['Column3']
+            
+            # Create JSON payload
+            payload = json.dumps({
+                'variable1': variable1,
+                'variable2': variable2,
+                'variable3': variable3
+            })
+            
+            # Call the API with cURL
+            api_url = 'http://example.com/api'  # Replace with your API 
+endpoint
+            post_data_to_api(api_url, payload)
+
+# Usage example
+csv_file_path = 'data.csv'  # Replace with your CSV file path
+csv_to_json(csv_file_path)
